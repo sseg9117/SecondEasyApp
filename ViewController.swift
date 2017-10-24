f9d49d932b0f5cc0ea9fd18d8e9c413dedf23eda
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-10-24 08:55:37 -0600

    added a mp3 with slider to adjust sound. Also added pause and play button. there is still an error with the pictures part of the app fixing it now.

--- ViewController.swift
+++ ViewController.swift
@@ -7,22 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController
     {
+    private var soundPlayer : AVAudioPlayer?
     private var imageCounter : Int = 0
+    private lazy var color : ColorTools = ColorTools()
+    
     @IBOutlet weak var imageFrame: UIImageView!
     @IBOutlet weak var soundSlider: UISlider!
     @IBOutlet weak var imageButton: UIButton!
     @IBOutlet weak var soundButton: UIButton!
     @IBOutlet weak var firstButton:UIButton!
-    
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
     }
-    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
     @IBAction func firstMethod(_ sender: UIButton)
     {
         if (firstButton.backgroundColor == .black)
@@ -51,37 +57,93 @@ class ViewController: UIViewController
         return newColor
     }
 
-    @IBAction func soundButton(_ sender: UIButton) {
+    @IBAction func soundButton(_ sender: UIButton)
+    {
     }
     @IBAction func changePicture(_ sender: UIButton)
     {
         switchImage()
     }
-    override func didReceiveMemoryWarning()
-    {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
     private func switchImage() -> Void
     {
         if (imageCounter > 2)
         {
             imageCounter = 0
         }
+        
         if (imageCounter == 0)
         {
             imageFrame.image = UIImage(named: "lel")
         }
-        else if (imageCounter == 1 )
+            
+        else if (imageCounter == 1)
         {
-            imageFrame.image = UIImage(named:"Poke")
+            imageFrame.image = UIImage(named: "Poke")
         }
         else
         {
-            imageFrame.image = UIImage(named:"Poke2")
+            imageFrame.image = UIImage(named: "images 2")
         }
-        
+        view.backgroundColor = color.createRandomColor();
         imageCounter += 1
+    }
+
+    @IBAction func soundPlay()
+    {
+        playMusicFile()
+    }
+    
+    private func playMusicFile() -> Void
+{
+    if let isPlaying = soundPlayer?.isPlaying
+    {
+        if (isPlaying)
+        {
+            soundPlayer?.pause()
+        }
+    }
+    
+    if (soundPlayer?.isPlaying)!
+    {
+        soundPlayer?.pause()
+    }
+        
+    else
+    {
+        soundPlayer?.play()
+    }
+}
+
+private func loadAudioFile() -> Void
+{
+    if let soundURL = NSDataAsset(name: "Sound")
+    {
+        do
+        {
+            try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try! AVAudioSession.sharedInstance().setActive(true)
+        
+            try soundPlayer = AVAudioPlayer(data: soundURL.data)
+        
+            soundSlider.maximumValue = Float ((soundPlayer?.duration)!)
+            Timer.scheduledTimer (timeInterval: 0.2, target: self, selector: (#selector(self.updateSlider)), userInfo: nil, repeats: true)
+        }
+        catch
+        {
+            print("Audio File Load Error")
+        }
+    }
+    
+    }
+    @objc private func updateSlider() -> Void
+    {
+        soundSlider.value = Float ((soundPlayer?.currentTime)!)
+    }
+    @IBAction func slidingSound(  sender: UISlider)
+    {
+        let seekTime = Double (soundSlider.value)
+        soundPlayer?.currentTime = seekTime
     }
 }
 
